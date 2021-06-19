e1f475a61de7f975848333d36bdfbf905094982c
Marshall Akpan <uimarshall@gmail.com>
2021-06-19 11:32:54 +0100

    Add MetaData, Home, Footer, and router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,19 @@
+import { BrowserRouter as Router, Route, NavLink } from 'react-router-dom';
+
 import './App.css';
+import Header from './components/layout/Header';
+import Home from './components/Home';
+import Footer from './components/layout/Footer';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
-    </div>
+    <Router>
+      <div className="App">
+        <Header />
+        <Route path="/" component={Home} exact />
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
