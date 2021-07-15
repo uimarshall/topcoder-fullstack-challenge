8af4b0ba96651a514ce125b3cab19f7162d17725
Marshall <uimarshall@gmail.com>
2021-07-15 12:12:10 +0100

    Add new product reviews actions and reducers.

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -7,6 +7,10 @@ import {
   GET_PRODUCT_DETAILS,
   PRODUCT_DETAILS_SUCCESS,
   PRODUCT_DETAILS_FAILURE,
+  NEW_REVIEW_REQUEST,
+  NEW_REVIEW_SUCCESS,
+  NEW_REVIEW_FAILURE,
+  NEW_REVIEW_RESET,
 } from './actionTypes';
 
 /*export const getProducts =
@@ -76,6 +80,35 @@ export const getProductDetails = (id) => async (dispatch) => {
   } catch (error) {
     dispatch({
       type: PRODUCT_DETAILS_FAILURE,
+      payload: error.response.data.message,
+    });
+  }
+};
+
+// New review
+export const newReview = (reviewData) => async (dispatch) => {
+  try {
+    dispatch({ type: NEW_REVIEW_REQUEST });
+
+    const config = {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    };
+
+    const { data } = await axios.put(
+      `/api/v1/products/review`,
+      reviewData,
+      config
+    );
+
+    dispatch({
+      type: NEW_REVIEW_SUCCESS,
+      payload: data.success,
+    });
+  } catch (error) {
+    dispatch({
+      type: NEW_REVIEW_FAILURE,
       payload: error.response.data.message,
     });
   }
--- frontend/src/reducers/productReducers.js
+++ frontend/src/reducers/productReducers.js
@@ -6,6 +6,10 @@ import {
   GET_PRODUCT_DETAILS,
   PRODUCT_DETAILS_SUCCESS,
   PRODUCT_DETAILS_FAILURE,
+  NEW_REVIEW_REQUEST,
+  NEW_REVIEW_SUCCESS,
+  NEW_REVIEW_FAILURE,
+  NEW_REVIEW_RESET,
 } from '../actions/actionTypes';
 
 const initialState = {
@@ -55,6 +59,44 @@ export const productDetailReducer = (
       return { loading: false, product: payload };
     case PRODUCT_DETAILS_FAILURE:
       return { ...state, error: payload, loading: false };
+    case CLEAR_ERRORS:
+      return {
+        ...state,
+        error: null,
+      };
+
+    default:
+      return state;
+  }
+};
+
+// New reviews reducer.
+export const newReviewReducer = (state = {}, action) => {
+  switch (action.type) {
+    case NEW_REVIEW_REQUEST:
+      return {
+        ...state,
+        loading: true,
+      };
+
+    case NEW_REVIEW_SUCCESS:
+      return {
+        loading: false,
+        success: action.payload,
+      };
+
+    case NEW_REVIEW_FAILURE:
+      return {
+        ...state,
+        error: action.payload,
+      };
+
+    case NEW_REVIEW_RESET:
+      return {
+        ...state,
+        success: false,
+      };
+
     case CLEAR_ERRORS:
       return {
         ...state,
